029b00429c0fb86ea5358214e1b979431684238f
Ziryt <lef332@gmail.com>
2023-08-11 20:59:48 +0300

    create update delete posts

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -20,4 +20,21 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        uu_form = UserUpdateForm(request.POST, instance=request.user)
+        pu_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if uu_form.is_valid() and pu_form.is_valid():
+            uu_form.save()
+            pu_form.save( )
+            messages.success(request, f'Account updated.')
+            return redirect('profile')
+    else:
+        uu_form = UserUpdateForm(instance=request.user)
+        pu_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': uu_form,
+        'p_form': pu_form
+    }
+
+    return render(request, 'users/profile.html', context)
